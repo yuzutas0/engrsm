b5c6adeb5aa7ee5240a1ae48897218bd33c5d26c
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-25 20:38:11 +0900

    replace word from my100tales to engrsm

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 # manage constant value
 module Constants
   # product name
-  PRODUCT_NAME_FOR_HEADER = ' My 100 tales'.freeze
+  PRODUCT_NAME_FOR_HEADER = 'Engrgm'.freeze
   PRODUCT_NAME_FOR_TITLE = PRODUCT_NAME_FOR_HEADER.delete(' ').freeze
 
   # magic number
