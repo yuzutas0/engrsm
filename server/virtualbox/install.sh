44578f5ada9a572aca64bf660e237c219ff1721c
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-04-23 20:29:10 +0900

    fix typo

--- server/virtualbox/install.sh
+++ server/virtualbox/install.sh
@@ -10,4 +10,4 @@ echo "password: vagrant"
 
 ssh vagrant@127.0.0.1 -p 2222 'bash -x ' < ./scripts/01_git.sh
 
-# TODO: use shellscipt at /home/vagrant/sync/
+# TODO: use shell script at /home/vagrant/sync/
