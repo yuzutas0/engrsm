b07edb8b05d6a910ff37a7d780b0bf065a07ec69
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-04-24 22:22:37 +0900

    add comment to setup elasticsearch at staging server

--- server/virtualbox/install_at_remote.sh
+++ server/virtualbox/install_at_remote.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 APP_NAME=engrsm
+HOST_NAME=staging.${APP_NAME}.com
 
 #sudo bash -x ./scripts/02_packages.sh
-#sudo DOMAIN_NAME=staging.${APP_NAME}.com ADMIN_EMAIL=${EMAIL} bash -x ./scripts/03_postfix.sh
+#sudo DOMAIN_NAME=${HOST_NAME} ADMIN_EMAIL=${EMAIL} bash -x ./scripts/03_postfix.sh
 #sudo ROOT_PASSWORD=${DB_ROOT_PASSWORD} USER=${APP_NAME} PASSWORD=${DB_PASSWORD} bash -x ./scripts/04_mariadb.sh
-sudo bash -x ./scripts/05_redis.sh
+#sudo bash -x ./scripts/05_redis.sh
+sudo APP=${APP_NAME} HOST=${HOST_NAME} bash -x ./scripts/06_elasticsearch.sh
 
-# TODO: elasticsearch
 # TODO: ruby
 # TODO: rails
 # TODO: nginx
--- server/virtualbox/scripts/06_elasticsearch.sh
+++ server/virtualbox/scripts/06_elasticsearch.sh
@@ -1,2 +1,83 @@
 #!/bin/bash
 
+# TODO: install elasticsearch
+
+## -----------------------
+## install java
+## -----------------------
+#
+#yum -y install java-1.8.0-openjdk-headless
+#
+#java -version
+## check: java command is enabled
+#
+## -----------------------
+## install elasticsearch
+## -----------------------
+## refs. https://www.elastic.co/guide/en/elasticsearch/reference/current/rpm.html
+#
+#rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+#
+#cat << _EOF > /etc/yum.repos.d/elasticsearch.repo
+#[elasticsearch-2.x]
+#name=Elasticsearch repository for 2.x packages
+#baseurl=http://packages.elastic.co/elasticsearch/2.x/centos
+#gpgcheck=1
+#gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+#enabled=1
+#_EOF
+#
+#yum -y install elasticsearch
+#
+#systemctl daemon-reload
+#systemctl start elasticsearch
+#systemctl enable elasticsearch
+#
+#systemctl status elasticsearch
+## check: active
+#
+## -----------------------
+## settings
+## -----------------------
+#
+#sed -i '$a cluster.name: ${APP}' /etc/elasticsearch/elasticsearch.yml
+#sed -i '$a node.name: ${HOST}' /etc/elasticsearch/elasticsearch.yml
+#
+#curl http://localhost:9200/_nodes/process?pretty
+## check: "mlockall" = false
+#ps -aef | grep elasticsearch | grep -v grep
+## check: -Xms256m -Xmx1g
+#free
+## check: Mem x total
+## calculate: Math.min((Mem x total / 2), 32GB) => ${memory}
+##   e.g. 256m, 1g
+#sed -i '$a ES_HEAP_SIZE=${memory}' /etc/sysconfig/elasticsearch
+#sed -i '$a MAX_LOCKED_MEMORY=unlimited' /etc/sysconfig/elasticsearch
+#sed -i '$a bootstrap.memory_lock: true' /etc/elasticsearch/elasticsearch.yml
+#
+#mkdir /etc/systemd/system/elasticsearch.service.d/
+#cat << _EOF > /etc/systemd/system/elasticsearch.service.d/limit.conf
+#[Service]
+#LimitMEMLOCK=infinity
+#_EOF
+#systemctl daemon-reload
+#
+#systemctl restart elasticsearch
+#systemctl status elasticsearch
+#
+#curl http://localhost:9200/_nodes/process?pretty
+## check: cluster_name, node_name, memory_lock
+#ps -aef | grep elasticsearch | grep -v grep
+## check: -Xms${memory} -Xmx${memory}
+#
+## -----------------------
+## install plugin
+## -----------------------
+#
+#cd /usr/share/elasticsearch
+#./bin/plugin install analysis-kuromoji
+#systemctl restart elasticsearch
+#
+#yum -y install jq
+#curl -X GET 'http://localhost:9200/_nodes/plugins' | jq
+## check: "plugins": [{"name": "analysis-kuromoji"
