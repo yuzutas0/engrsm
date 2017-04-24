e2e7bf9fd25320d9c51e9dbbe218524b9ef87bf6
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-04-24 22:12:59 +0900

    setup redis at staging server

--- server/virtualbox/install_at_remote.sh
+++ server/virtualbox/install_at_remote.sh
@@ -4,10 +4,9 @@ APP_NAME=engrsm
 
 #sudo bash -x ./scripts/02_packages.sh
 #sudo DOMAIN_NAME=staging.${APP_NAME}.com ADMIN_EMAIL=${EMAIL} bash -x ./scripts/03_postfix.sh
-sudo ROOT_PASSWORD=${DB_ROOT_PASSWORD} USER=${APP_NAME} PASSWORD=${DB_PASSWORD} bash -x ./scripts/04_mariadb.sh
+#sudo ROOT_PASSWORD=${DB_ROOT_PASSWORD} USER=${APP_NAME} PASSWORD=${DB_PASSWORD} bash -x ./scripts/04_mariadb.sh
+sudo bash -x ./scripts/05_redis.sh
 
-# TODO: mariadb or mysql
-# TODO: redis
 # TODO: elasticsearch
 # TODO: ruby
 # TODO: rails
--- server/virtualbox/scripts/05_redis.sh
+++ server/virtualbox/scripts/05_redis.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+yum install -y epel-release
+yum --enablerepo=epel install -y redis
+
+systemctl start redis
+systemctl enable redis
+
+redis-cli ping
