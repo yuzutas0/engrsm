b6cbb3f186686911be4de7999ccce491319c22fe
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-05-06 23:30:32 +0900

    refactor

--- server/virtualbox/install_at_local.sh
+++ server/virtualbox/install_at_local.sh
@@ -3,6 +3,9 @@
 # execute first at application root directory
 # $ cd server/virtualbox && bash -x ./install_at_local.sh
 
+work_dir='/home/vagrant/sync'
+capistrano_dir='/home/vagrant/engrsm'
+
 # ================================
 # setup vagrant
 # ================================
@@ -19,10 +22,13 @@ echo "*** password: vagrant"
 echo -n "*** Is it OK to continue? [yes/no]"
 read answer
 
-ssh vagrant@127.0.0.1 -p 2222 'find sync -name "*.sh" | xargs chmod +x'
-ssh vagrant@127.0.0.1 -p 2222 'USER_NAME=vagrant HOST_NAME=127.0.0.1 bash -x ./sync/scripts/01_code.sh'
-ssh vagrant@127.0.0.1 -p 2200 'find sync -name "*.sh" | xargs chmod +x'
-ssh vagrant@127.0.0.1 -p 2200 'USER_NAME=vagrant HOST_NAME=127.0.0.1 bash -x ./sync/scripts/01_code.sh'
+ssh vagrant@127.0.0.1 -p 2222 "find ${work_dir} -name "*.sh" | xargs chmod +x"
+ssh vagrant@127.0.0.1 -p 2222 "sudo bash -x ${work_dir}/scripts/00_provision.sh"
+ssh vagrant@127.0.0.1 -p 2222 "USER_NAME=vagrant HOST_NAME=127.0.0.1 bash -x ${work_dir}/scripts/01_code.sh"
+
+ssh vagrant@127.0.0.1 -p 2200 "find ${work_dir} -name "*.sh" | xargs chmod +x"
+ssh vagrant@127.0.0.1 -p 2200 "sudo bash -x ${work_dir}/scripts/00_provision.sh"
+ssh vagrant@127.0.0.1 -p 2200 "USER_NAME=vagrant HOST_NAME=127.0.0.1 bash -x ${work_dir}/scripts/01_code.sh"
 
 # ================================
 # setting custom variables
@@ -50,14 +56,14 @@ read answer
 # ================================
 
 vagrant ssh engrsm -c \
-"cd /home/vagrant/sync && EMAIL=${email} DB_ROOT_PASSWORD=${db_root_password} DB_PASSWORD=${db_password} \
+"cd ${work_dir} && EMAIL=${email} DB_ROOT_PASSWORD=${db_root_password} DB_PASSWORD=${db_password} \
 bash -x ./install_at_server1.sh"
 
 vagrant ssh deploy -c \
-"cd /home/vagrant/sync && DB_PASSWORD=${db_password} bash -x ./install_at_deploy.sh"
+"cd ${work_dir} && DB_PASSWORD=${db_password} bash -x ./install_at_deploy.sh"
 
 vagrant ssh engrsm -c \
-"cd /home/vagrant/sync && bash -x ./install_at_server2.sh"
+"cd ${work_dir} && bash -x ./install_at_server2.sh"
 
-vagrant ssh deploy -c "cd /home/vagrant/engrsm && bundle exec cap production deploy"
+vagrant ssh deploy -c "cd ${capistrano_dir} && bundle exec cap production deploy"
 vagrant ssh engrsm -c "sudo systemctl restart nginx"
